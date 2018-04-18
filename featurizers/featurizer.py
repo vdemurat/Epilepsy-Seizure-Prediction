b60b1ef882adc17419b181a8fe53ced83ebf19fc
msaadl <msaad.lahlou@gmail.com>
2018-04-18 12:45:15 -0400

    changed featurizer

--- featurizers/featurizer.py
+++ featurizers/featurizer.py
@@ -9,13 +9,21 @@ import numpy as np
 
 
 def featurize(X,cutoff=False,T=15):
+    X=np.transpose(X)
     N=len(X)
-    sp=10*60/T
-    separate=np.split(X,sp)
+    sp=int(10*60/T)
+    print(type(sp))
+    print(X.shape)
+    split_array=np.floor(np.linspace(0,N,sp+1))
+    split_array=split_array[1:sp]
+    split_array=split_array.astype('int')
+    separate=np.split(X,split_array)
     e=[]
     for tranche in separate: 
         energies=[]
+        tranche=np.transpose(tranche)
         for i in range(16): 
+            print(tranche[i].shape)
             FT=np.fft.fft(tranche[i])
             energy=np.linalg.norm(FT)**2
             energies.append(energy)
@@ -35,12 +43,19 @@ def featurize(X,cutoff=False,T=15):
 
 
 def featurize_weak(X,cutoff=False,T=15): 
+    X=np.transpose(X)
     N=len(X)
-    sp=10*60/T
-    separate=np.split(X,sp)
+    sp=int(10*60/T)
+    print(type(sp))
+    print(X.shape)
+    split_array=np.floor(np.linspace(0,N,sp+1))
+    split_array=split_array[1:sp]
+    split_array=split_array.astype('int')
+    separate=np.split(X,split_array)
     e=[]
     for tranche in separate: 
         energies=[]
+        print(tranche.shape)
         for i in range(16): 
             FT=np.fft.fft(tranche[i])
             energy=np.linalg.norm(FT)**2
@@ -51,7 +66,25 @@ def featurize_weak(X,cutoff=False,T=15):
     total_energy=compute_norm(FF)
     mean_energy=np.mean(e,axis=0)
     e=e.reshape(-1)
-    return np.hstack(total_energy,np.array(e),mean_energy)    
+    return np.hstack((total_energy,np.array(e),mean_energy))    
+
+
+def featurize_FF(X,cutoff=50): 
+    transforms=[]
+    for column in X: 
+        FF=np.fft.fft(X)
+        FFshort=FF[0:cutoff]
+        transforms.append(FFshort)
+
+
+
+
+def compute_norm(signal):
+    signal=np.transpose(signal)
+    norms=[]
+    for x in signal:
+        norms.append(np.linalg.norm(x)**2)
+    return np.mean(norms)
 
 
 
