ec003787fed9fabbaa0dbf2a2634d8ae2ce4aacc
msaadl <msaad.lahlou@gmail.com>
2018-04-18 15:44:32 -0400

    improvement in featurizer robustness

--- featurizers/featurizer.py
+++ featurizers/featurizer.py
@@ -12,20 +12,20 @@ def featurize(X,cutoff=False,T=15):
     X=np.transpose(X)
     N=len(X)
     sp=int(10*60/T)
-    print(type(sp))
-    print(X.shape)
+    #print(type(sp))
+    #print(X.shape)
     split_array=np.floor(np.linspace(0,N,sp+1))
     split_array=split_array[1:sp]
     split_array=split_array.astype('int')
     separate=np.split(X,split_array)
     e=[]
-    print(separate[0].shape)
+    #print(separate[0].shape)
     for tranche in separate: 
         energies=[]
         tranche=np.transpose(tranche)
-        for i in range(16): 
+        for i in range(15): 
             #print(tranche[i].shape)
-            print(tranche.shape)
+            #print(tranche.shape)
             FT=np.fft.fft(tranche[i])
             energy=np.log(np.linalg.norm(FT)**2)
             energies.append(energy)
